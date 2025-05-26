a72bd9316c3da841a615b3083c73a26c264a8794
Ali Kaan Guven <alikaan.guven@gmail.com>
2025-05-26 13:33:55 +0200

    Update in Zheyu Wu's repo.

--- Plotter/testZ/submit_to_cpu_short.sh
+++ Plotter/testZ/submit_to_cpu_short.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-# Usage: sbatch submit_to_cpu.sh "python3 autoplotter.py  --sample wjetstolnuht0100_2018  --output /scratch-cbe/users/alikaan.gueven/2018_limits --config configs/calc_limits.yaml --lumi 59800 --json CustomNanoAOD_v3_bkg.json --datalabel CustomNanoAOD"
+# Usage: sbatch submit_to_cpu.sh "python3 autoplotter.py  --sample wjetstolnuht0100_2018  --output /scratch-cbe/users/zhenyu.wu/2018_limits --config configs/calc_limits.yaml --lumi 59800 --json CustomNanoAOD_v3_bkg.json --datalabel CustomNanoAOD"
 
 
 #SBATCH --job-name=autoplotter
-#SBATCH --output=/scratch-cbe/users/alikaan.gueven/job_outs/job_%j.out 
+#SBATCH --output=/scratch-cbe/users/zhenyu.wu/job_outs/job_%j.out 
 #SBATCH --ntasks 1 
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=250M 
