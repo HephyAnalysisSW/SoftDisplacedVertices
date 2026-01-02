951f0fa17f7a9700a25928d8718b13b77b47ad90
Ang Li <Ang.Li@oeaw.ac.at>
2026-01-02 17:15:15 +0100

    cpp code for JERC

---
+++ JERC/interface/Year.h
@@ -0,0 +1,8 @@
+#ifndef JERC_Year_h
+#define JERC_Year_h
+
+namespace SoftDV {
+    std::pair<std::string,std::string> getEra(double run_number);
+}
+
+#endif
---
+++ JERC/python/JecAppliedJetProducer_cfi.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+jecAppliedJetProducer = cms.EDProducer(
+    "JecAppliedJetProducer",
+    isDebug = cms.bool(False),  # set True to dump [JERC DEBUG] logs
+
+    Jets = cms.PSet(
+        srcAK4 = cms.InputTag("slimmedJets"),          # pat::JetCollection
+        rho    = cms.InputTag("fixedGridRhoFastjetAll"),
+        Year   = cms.string("2024"),
+        IsData = cms.bool(False),
+
+        # --- Choose one of: "Nominal", "JES", "JER"
+        SystKind    = cms.string("Nominal"),
+
+        # If SystKind == "JES"
+        JesSystName = cms.string("AbsoluteStat"),  # correction set key
+        JesSystVar  = cms.string("Up"),            # "Up" | "Down"
+
+        # If SystKind == "JER"
+        JerVar      = cms.string("nom"),           # "nom" | "up" | "down"
+        JerRegion   = cms.PSet(                    # optional gate
+            etaMin = cms.double(0.0),
+            etaMax = cms.double(999.0),
+            ptMin  = cms.double(0.0),
+            ptMax  = cms.double(1.0e9),
+        ),
+        JecConfig = cms.FileInPath("SoftDisplacedVertices/JERC/data/JecConfigAK4.json"),
+        JerToolConfig = cms.FileInPath("SoftDisplacedVertices/JERC/data/jer_smear.json.gz"),
+    ),
+)
+
+
